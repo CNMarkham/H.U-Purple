7cbe3947ff6f410f9cecb4698f82986ada205b4a
Roosham <markhamonca@codeninjas.com>
2023-09-23 11:57:17 -0400

    H.U - Breakout
    Breakout. step 32

--- H.U - Breakout (1976)/Assets/GameManager.cs
+++ H.U - Breakout (1976)/Assets/GameManager.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
+{ 
+    public float xSpace;
+    public float xOffset;
+
     public GameObject brickPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        //for ()
-        //{
-        //    Instantiate(brickPrefab, Vector2.zero, Quaternion.identity);
-        //}
+        // a for loop has 3 parts a iterator, (or counting) variable, an end condition, and increment
+        for (int x = 0; x < 10; x++)
+        {   // the instantiate function has 3 parameters: the object you are spawning, where your spawning it, and the rotation.
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 3), Quaternion.identity);
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 3.75f), Quaternion.identity);
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 4.5f), Quaternion.identity);
+
+        }
     }
     // Update is called once per frame
 }
