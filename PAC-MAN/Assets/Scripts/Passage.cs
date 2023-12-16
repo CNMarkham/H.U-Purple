a133025e05faf1dfe41197c0a5ed79571aca81cf
Miles <markhamonca@codeninjas.com>
2023-12-16 11:59:16 -0500

    HU pacman step 85
    step 85

--- PAC-MAN/Assets/Scripts/Passage.cs
+++ PAC-MAN/Assets/Scripts/Passage.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Passage : MonoBehaviour
 {
     public Transform teleportPosition;
-    private void OnTriggerEnter(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-       
+        Vector3 tempPos = teleportPosition.position;
+        tempPos.z = collision.transform.position.z;
+        collision.transform.position = tempPos;
     }
     // Start is called before the first frame update
     void Start()
