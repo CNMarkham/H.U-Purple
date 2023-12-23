1024489a89fe9ec59404373787e57f4aed0b6bd8
CNMarkham <132835392+CNMarkham@users.noreply.github.com>
2023-12-23 12:00:44 -0500

    step 101
    sd

---
+++ PAC-MAN/Assets/Scripts/Ghost.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : Movement
+{
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+    public GameObject white;
+    public bool atHome;
+    public float homeDuration;
+    private bool frightened;
+    // Start is called before the first frame update
+    
+    private void Awake()
+    {
+        
+    }
+
+    protected void ChildUpdate()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+
+        if (node != null)
+        {
+            int index = Random.Range(0, node.availableDirections.Count);
+
+            SetDirection(node.availableDirections[index]);
+        }
+    }
+
+    private void LeaveHome()
+    {
+
+    }
+
+    public void Frighten()
+    {
+
+    }
+
+    private void Flash()
+    {
+
+    }
+
+    private void Reset()
+    {
+
+    }
+
+    // Update is called once per frame
+
+}
